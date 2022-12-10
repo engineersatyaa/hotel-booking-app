3189874dc1f0d7d50725e0fbd3273ea499a00991
SATYA PAL SINGH <engineersatyaa@gmail.com>
2022-12-10 16:31:21 +0530

    Important Notes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Login from "./pages/Login";
 const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      {/* Important:- You have to put "basename" attribute with "process.env.PUBLIC_URL" value when you have .env file in your project otherwise when you deploy your website on github it will show you blank pages in live website. */}
+
+      {/* Another note to remember is that when you deploy ur website on github them make sure ur "images URL" must have newly created github repository name in images URL otherwise images will not be shown after deployment. */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<HotelList />} />
